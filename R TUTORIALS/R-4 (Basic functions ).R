f297ac6a210e3fccd07668c591be118c7b4a6e4c
Megha Garg <43913641+miss-jain-16@users.noreply.github.com>
2020-10-15 09:31:21 +0530

    Day-2(r)

---
+++ R TUTORIALS/R-4 (Basic functions ).R
@@ -0,0 +1,19 @@
+#overgrown claculator
+2+2
+2+.2
+2+3*5
+bb<-matrix(1:9,ncol=3)
+bb
+coordinates <- expand.grid(1:100, 1:100)
+coordinates
+rep(1,5)
+rep(1:5,each=3)
+seq(1:5)
+1:5
+
+v<-TRUE
+print(class(v))
+
+v <- 2+5i
+print(class(v))
+
